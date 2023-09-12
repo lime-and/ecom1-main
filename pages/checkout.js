eea9c600e9fb3eab456718665ac1a0c808901d1b
lime <michaelliman21@gmail.com>
2023-09-12 11:27:03 +0900

    checkout src update

--- pages/checkout.js
+++ pages/checkout.js
@@ -50,7 +50,7 @@ export default function CheckoutPage() {
         return (
         <div className="flex mb-5 items-center" key={productInfo._id}>
           <div className="bg-gray-100 p-3 rounded-xl shrink-0" style={{boxShadow:'inset 1px 0px 10px 10px rgba(0,0,0,0.1)'}}>
-            <img className="w-24" src={productInfo.picture} alt=""/>
+            <img className="w-24" src={`/products/${productInfo.picture}`} alt=""/>
           </div>
           <div className="pl-4 items-center">
             <h3 className="font-bold text-lg">{productInfo.name}</h3>
